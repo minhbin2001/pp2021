2ce844dad7a47eacaa66c567a62e68bb7fca78a3
minhbin2001 <79209238+minhbin2001@users.noreply.github.com>
2021-04-24 12:35:14 +0700

    Update 1.student.mark.py

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,1 +1,18 @@
+#creating an empty list 
+lst1 =[]
+lst 2 = []
+lst 3= []
+# number of Student 
+nos = int(input("Enter the number of students in a class: "))
+id = int(input("Enter the student id :"))
+name = input("Enter the name of the student :")
+dob = int("Enter the student date of birth:")
 
+#list course
+noc = int(input("Enter number of courses:")
+for i in range(0,n):
+print("Enter course name:" . format(i +1))
+ele = [input(),int(input()]
+list3.append(ele)
+print("The entered list is: \n,list3)
+# select a course ; 
